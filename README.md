a40b901eb21ba73272e4a510e82b63d46b713b2b
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2026-02-19 14:35:08 +0100

    Added code

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Astro SSG Example
+
+Install: `pnpm install`
+
+Run dev server: `pnpm run dev`
+
+Example urls:
+- http://localhost:4321/pages/2097152
+
+Build static site: `pnpm run build`
---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+      format: 'file',  // for foo.astro output foo.html instead of foo/index.html
+      concurrency: 2,
+  }
+});

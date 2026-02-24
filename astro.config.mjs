9e9789402899d4618afd8e14df13fac19afcd87c
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2026-02-24 19:04:48 +0100

    Updated to astro@6.0.0-beta.15

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-      format: 'file',  // for foo.astro output foo.html instead of foo/index.html
-      concurrency: 2,
-  }
+	build: {
+		format: "file", // for foo.astro output foo.html instead of foo/index.html
+		concurrency: 2,
+	},
 });
